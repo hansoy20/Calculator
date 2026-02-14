1e7768d335ab75138061945f2ecc74ce552fbc1c
thegreatpaolo <gianoga3@gmail.com>
2026-02-14 09:01:19 +0000

    Gian Oga :Updated the code

--- ParallelPayrollSystem/payroll_system.py
+++ ParallelPayrollSystem/payroll_system.py
@@ -1,14 +1,13 @@
-# Member 1 (Leader): Initial Setup & Shared Data
 import concurrent.futures
 import threading
 
-# Given Employee Data [cite: 21-26]
+# Given Employee Data 
 employees = [
     ("Alice", 25000), ("Bob", 32000), ("Charlie", 28000),
     ("Diana", 40000), ("Edward", 35000)
 ]
 
-# Shared Deduction Rates [cite: 16-19]
+# Shared Deduction Rates 
 RATES = {
     "SSS": 0.045,
     "PhilHealth": 0.025,
@@ -18,4 +17,4 @@ RATES = {
 
 if __name__ == "__main__":
     print("Payroll System Project Initialized.")
-    # Members 2 & 3 will add their execution logic below this line
+    
