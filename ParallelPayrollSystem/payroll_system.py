38eb3461791b468a5744c391822fe7910767389f
RAHHHKETT <austriakeith08@gmail.com>
2026-02-14 10:22:15 +0000

    keith Austria :Added task Parallelism logic

--- ParallelPayrollSystem/payroll_system.py
+++ ParallelPayrollSystem/payroll_system.py
@@ -14,3 +14,30 @@ RATES = {
     "Tax": 0.10
 }
 
+def compute_sss(salary): return salary * RATES["SSS"]
+def compute_philhealth(salary): return salary * RATES["PhilHealth"]
+def compute_pagibig(salary): return salary * RATES["Pag-IBIG"]
+def compute_tax(salary): return salary * RATES["Tax"]
+
+def run_task_parallelism(name, salary):
+    print(f"\n>>> Part A: Task Parallelism for {name}")
+    
+    tasks = {
+        "SSS": compute_sss,
+        "PhilHealth": compute_philhealth,
+        "Pag-IBIG": compute_pagibig,
+        "Withholding Tax": compute_tax
+    }
+    
+    total_deduction = 0
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        # Submit tasks concurrently and return Future objects
+        futures = {executor.submit(func, salary): label for label, func in tasks.items()}
+        
+        for future in concurrent.futures.as_completed(futures):
+            label = futures[future]
+            amount = future.result()
+            total_deduction += amount
+            print(f" - {label}: {amount:.2f} | Thread: {threading.current_thread().name}")
+            
+    print(f"Total Deduction for {name}: {total_deduction:.2f}")
